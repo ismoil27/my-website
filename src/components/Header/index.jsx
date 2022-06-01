e5d6c76ea9a3c7a4444a59a3ab74e7ffa355d680
Ismoiljon (Jon) <abduraimovismoil98@gmail.com>
2022-06-01 22:52:50 +0900

    changed cv

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -11,7 +11,7 @@ const Header = () => {
       <Container className="header__container">
         <h5>Hello, I'm</h5>
         <h1>Ismoiljon</h1>
-        <h5 className="text-light">React Developer</h5>
+        <h5 className="text-light">Front End Developer</h5>
         <CTA />
         <HeaderIcons />
         <ImageWrapper className="me">
